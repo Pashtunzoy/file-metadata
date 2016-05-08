abdd750e90e0436122cf0c82aa14d170710b9e24
Asep Norzai <asifkhannoorzai@gmail.com>
2016-05-08 10:21:48 +0000

    Had a console.log statement

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,6 @@ $(document).submit('#upload-form', function(e) {
         processData: false,
         url: '/',						
         success: function(data) {
-            console.log(data);
             var loadFile = function() {
              return "<img src=" + URL.createObjectURL($('.file')[0].files[0]) + ">";
             };
@@ -20,7 +19,7 @@ $(document).submit('#upload-form', function(e) {
             var printItToPage = '<div class="row">\n  <div class=" col-xs-12">\n    <div class="thumbnail">\n    ' + (imageOrNot === 'image' ? loadFile() : '') + '\n      <div class="caption">\n        <p>File Name: ' + data.originalname + '</p>\n        <p>Size(bytes): ' + data.size + '</p>\n        <p>File Type: ' + data.mimetype + '</p>\n        <p>Encoding: ' + data.encoding + '</p>\n      </div>\n    </div>\n  </div>\n</div>';
                                 
             $('#data-here').html(printItToPage);
-            alert('File Name: ' + data.originalname+', ' +'File Size: '+ data.size);
+            // alert('File Name: ' + data.originalname+', ' +'File Size: '+ data.size);
         }
         });
       }
